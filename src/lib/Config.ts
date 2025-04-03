c9654dec8038c98221ae548dc478d179d1335784
Garfonso <garfonso@mobo.info>
2025-04-03 11:50:08 +0200

    config: add read config method

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -1,5 +1,19 @@
+import {readFile} from 'node:fs/promises';
+import {ParserConfig} from './ParserConfig';
+import {Logger} from 'sitka';
 
 export class Config {
-    constructor() {
+    parsers: Array<ParserConfig> = [];
+    outputPath: string = '.';
+
+    async readConfig(path = './config.json', logger: Logger) {
+        try {
+            const configString = await readFile(path, 'utf-8');
+            const configObj = JSON.parse(configString);
+            this.parsers = configObj.parsers || [];
+            this.outputPath = configObj.outputPath || '.';
+        } catch (e) {
+            logger.error('Error reading config file: ' + e);
+        }
     }
 }
